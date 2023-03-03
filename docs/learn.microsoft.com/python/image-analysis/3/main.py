3ad8e434d40cbdd5835209bcdcd69e1e04141079
Darren Cohen <darren.cohen@microsoft.com>
2023-03-03 14:47:40 -0800

    Use env variables for Python. Be consistant on string quotes

--- docs/learn.microsoft.com/python/image-analysis/3/main.py
+++ docs/learn.microsoft.com/python/image-analysis/3/main.py
@@ -2,16 +2,18 @@
 # Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 
 # <snippet-single>
+import os
 import azure.ai.vision as sdk
 
-service_options = sdk.VisionServiceOptions("PASTE_YOUR_COMPUTER_VISION_ENDPOINT_HERE", "PASTE_YOUR_COMPUTER_VISION_SUBSCRIPTION_KEY_HERE")
+service_options = sdk.VisionServiceOptions(
+     os.environ["VISION_ENDPOINT"],
+     os.environ["VISION_KEY"])
 
 vision_source = sdk.VisionSource(url="https://csspeechstorage.blob.core.windows.net/drop/TestData/images/ocr-sample.jpg")
 
 analysis_options = sdk.ImageAnalysisOptions()
 
-# Set your custom model name here
-analysis_options.model_name = 'MyCustomModelName'
+analysis_options.model_name = "MyCustomModelName"
 
 image_analyzer = sdk.ImageAnalyzer(service_options, vision_source, analysis_options)
 
@@ -20,14 +22,14 @@ result = image_analyzer.analyze()
 if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
 
     if result.custom_objects is not None:
-        print(' Custom Objects:')
+        print(" Custom Objects:")
         for object in result.custom_objects:
-            print('   \'{}\', {} Confidence: {:.4f}'.format(object.name, object.bounding_box, object.confidence))
+            print("   '{}', {} Confidence: {:.4f}".format(object.name, object.bounding_box, object.confidence))
 
     if result.custom_tags is not None:
-        print(' Custom Tags:')
+        print(" Custom Tags:")
         for tag in result.custom_tags:
-            print('   \'{}\', Confidence {:.4f}'.format(tag.name, tag.confidence))
+            print("   '{}', Confidence {:.4f}".format(tag.name, tag.confidence))
 
 elif result.reason == sdk.ImageAnalysisResultReason.ERROR:
 
