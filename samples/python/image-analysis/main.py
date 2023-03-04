a49fa8efde3979784c136fc9100a60eca54dbe3d
Darren Cohen <darren.cohen@microsoft.com>
2023-03-03 16:43:05 -0800

    First

--- samples/python/image-analysis/main.py
+++ samples/python/image-analysis/main.py
@@ -14,29 +14,29 @@ import samples
 
 def print_usage():
     print()
-    print(' To run the samples:')
+    print(" To run the samples:")
     print()
-    print('   python main.py [--key|-k <your-key>] [--endpoint|-e <your-endpoint>]')
+    print("   python main.py [--key|-k <your-key>] [--endpoint|-e <your-endpoint>]")
     print()
-    print(' Where:')
-    print('   <your-key> - A computer vision key you get from your Azure portal.')
-    print('     It should be a 32-character HEX number.')
-    print('   <your-endpoint> - A computer vision endpoint you get from your Azure portal.')
-    print('     It should have the form:')
-    print('     https://<your-computer-vision-resource-name>.cognitiveservices.azure.com')
+    print(" Where:")
+    print("   <your-key> - A computer vision key you get from your Azure portal.")
+    print("     It should be a 32-character HEX number.")
+    print("   <your-endpoint> - A computer vision endpoint you get from your Azure portal.")
+    print("     It should have the form:")
+    print("     https://<your-computer-vision-resource-name>.cognitiveservices.azure.com")
     print()
-    print(' As an alternative to specifying the above command line arguments, you can specify')
-    print(' these environment variables: {} and/or {}.'.format(secrets.ENVIRONMENT_VARIABLE_KEY,
+    print(" As an alternative to specifying the above command line arguments, you can specify")
+    print(" these environment variables: {} and/or {}.".format(secrets.ENVIRONMENT_VARIABLE_KEY,
                                                                secrets.ENVIRONMENT_VARIABLE_ENDPOINT))
     print()
-    print(' To get this usage help, run:')
+    print(" To get this usage help, run:")
     print()
-    print('   python main.py --help|-h')
+    print("   python main.py --help|-h")
     print()
 
 
 def select():
-    abort_key = 'Ctrl-Z' if "Windows" == platform.system() else 'Ctrl-D'
+    abort_key = "Ctrl-Z" if "Windows" == platform.system() else "Ctrl-D"
 
     sample_functions = [
         samples.get_all_results,
@@ -44,7 +44,7 @@ def select():
         samples.get_custom_model_results
     ]
 
-    print(' Select sample or {} to abort:'.format(abort_key))
+    print(" Select sample or {} to abort:".format(abort_key))
     print()
     for i, fun in enumerate(sample_functions):
         print(" {}: {}\n{}".format(i, fun.__name__, fun.__doc__))
@@ -58,21 +58,21 @@ def select():
         print(e)
         return
 
-    print('You selected: {}'.format(selected_function))
+    print("You selected: {}".format(selected_function))
     try:
         selected_function()
     except Exception as e:
-        print('Error running sample: {}'.format(e))
+        print("Error running sample: {}".format(e))
 
     print()
 
 
 print()
-print(' Azure AI Vision SDK - Image Analysis Samples')
+print(" Azure AI Vision SDK - Image Analysis Samples")
 print()
 
 for arg in sys.argv:
-    if arg in ['--help', '-h']:
+    if arg in ["--help", "-h"]:
         print_usage()
         sys.exit(0)
 
