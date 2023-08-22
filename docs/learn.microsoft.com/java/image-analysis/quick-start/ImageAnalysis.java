dd118ff1bcf567a76f77b1199bb8eebe5b5087ee
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2023-08-22 16:14:46 -0700

    First

---
+++ docs/learn.microsoft.com/java/image-analysis/quick-start/ImageAnalysis.java
@@ -0,0 +1,80 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+// This code is integrated into this public document:
+// https://learn.microsoft.com/azure/ai-services/computer-vision/quickstarts-sdk/image-analysis-client-library-40?tabs=visual-studio%2Cwindows&pivots=programming-language-java#analyze-image
+
+
+// <snippet_single>
+import java.net.URL;
+import java.util.EnumSet;
+
+import com.azure.ai.vision.common.*;
+import com.azure.ai.vision.imageanalysis.*;
+
+public class ImageAnalysis {
+
+    public static void main(String[] args) {
+
+        try (
+            VisionServiceOptions serviceOptions = new VisionServiceOptions(
+                new URL(System.getenv("VISION_ENDPOINT")),
+                System.getenv("VISION_KEY"));
+
+            VisionSource imageSource = VisionSource.fromUrl(
+                new URL("https://learn.microsoft.com/azure/cognitive-services/computer-vision/media/quickstarts/presentation.png"));
+
+            ImageAnalysisOptions analysisOptions = new ImageAnalysisOptions()) {
+
+            analysisOptions.setFeatures(EnumSet.of(ImageAnalysisFeature.CAPTION, ImageAnalysisFeature.TEXT));
+
+            analysisOptions.setLanguage("en");
+
+            analysisOptions.setGenderNeutralCaption(true);
+
+            try (
+                ImageAnalyzer analyzer = new ImageAnalyzer(serviceOptions, imageSource, analysisOptions);
+
+                ImageAnalysisResult result = analyzer.analyze()) {
+
+                if (result.getReason() == ImageAnalysisResultReason.ANALYZED) {
+
+                    if (result.getCaption() != null) {
+                        System.out.println(" Caption:");
+                        System.out.println("   \"" + result.getCaption().getContent() + "\", Confidence "
+                                + String.format("%.4f", result.getCaption().getConfidence()));
+                    }
+
+                    if (result.getText() != null) {
+                        System.out.println(" Text:");
+                        for (DetectedTextLine line : result.getText()) {
+                            String pointsToString = "{" + String.join(",",
+                                    line.getBoundingPolygon().stream().map(Object::toString).toArray(String[]::new))
+                                    + "}";
+                            System.out.println(
+                                    "   Line: '" + line.getContent() + "', Bounding polygon " + pointsToString);
+
+                            for (DetectedTextWord word : line.getWords()) {
+                                pointsToString = "{" + String.join(",",
+                                        word.getBoundingPolygon().stream().map(Object::toString).toArray(String[]::new))
+                                        + "}";
+                                System.out.println(
+                                        "     Word: '" + word.getContent() + "', Bounding polygon " + pointsToString +
+                                                ", Confidence " + String.format("%.4f", word.getConfidence()));
+                            }
+                        }
+                    }
+                } else { // result.Reason == ImageAnalysisResultReason.Error
+                    ImageAnalysisErrorDetails errorDetails = ImageAnalysisErrorDetails.fromResult(result);
+                    System.out.println(" Analysis failed.");
+                    System.out.println("   Error reason: " + errorDetails.getReason());
+                    System.out.println("   Error code: " + errorDetails.getErrorCode());
+                    System.out.println("   Error message: " + errorDetails.getMessage());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+// </snippet_single>
---
+++ docs/learn.microsoft.com/python/image-analysis/how-to/main.py
@@ -0,0 +1,112 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+#
+# This code is integrated into this public document:
+# https://learn.microsoft.com/azure/ai-services/computer-vision/how-to/call-analyze-image-40?pivots=programming-language-python
+
+import os
+
+# <vision_service_options>
+import azure.ai.vision as sdk
+
+service_options = sdk.VisionServiceOptions(os.environ["VISION_ENDPOINT"],
+                                           os.environ["VISION_KEY"])
+# </vision_service_options>
+
+# <vision_source>
+vision_source = sdk.VisionSource(
+    url="https://learn.microsoft.com/azure/cognitive-services/computer-vision/media/quickstarts/presentation.png")
+# </vision_source>
+
+# <visual_features>
+analysis_options = sdk.ImageAnalysisOptions()
+
+analysis_options.features = (
+    sdk.ImageAnalysisFeature.CROP_SUGGESTIONS |
+    sdk.ImageAnalysisFeature.CAPTION |
+    sdk.ImageAnalysisFeature.DENSE_CAPTIONS |
+    sdk.ImageAnalysisFeature.OBJECTS |
+    sdk.ImageAnalysisFeature.PEOPLE |
+    sdk.ImageAnalysisFeature.TEXT |
+    sdk.ImageAnalysisFeature.TAGS
+)
+# </visual_features>
+
+# <cropping_aspect_ratios>
+analysis_options.cropping_aspect_ratios = [0.9, 1.33]
+# </cropping_aspect_ratios>
+
+# <language>
+analysis_options.language = "en"
+# </language>
+
+# <gender_neutral_caption>
+analysis_options.gender_neutral_caption = True
+# </gender_neutral_caption>
+
+# <analyze>
+image_analyzer = sdk.ImageAnalyzer(service_options, vision_source, analysis_options)
+
+result = image_analyzer.analyze()
+
+if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+
+    print(" Image height: {}".format(result.image_height))
+    print(" Image width: {}".format(result.image_width))
+    print(" Model version: {}".format(result.model_version))
+
+    if result.caption is not None:
+        print(" Caption:")
+        print("   '{}', Confidence {:.4f}".format(result.caption.content, result.caption.confidence))
+
+    if result.dense_captions is not None:
+        print(" Dense Captions:")
+        for caption in result.dense_captions:
+            print("   '{}', {}, Confidence: {:.4f}".format(caption.content, caption.bounding_box, caption.confidence))
+
+    if result.objects is not None:
+        print(" Objects:")
+        for object in result.objects:
+            print("   '{}', {}, Confidence: {:.4f}".format(object.name, object.bounding_box, object.confidence))
+
+    if result.tags is not None:
+        print(" Tags:")
+        for tag in result.tags:
+            print("   '{}', Confidence {:.4f}".format(tag.name, tag.confidence))
+
+    if result.people is not None:
+        print(" People:")
+        for person in result.people:
+            print("   {}, Confidence {:.4f}".format(person.bounding_box, person.confidence))
+
+    if result.crop_suggestions is not None:
+        print(" Crop Suggestions:")
+        for crop_suggestion in result.crop_suggestions:
+            print("   Aspect ratio {}: Crop suggestion {}"
+                  .format(crop_suggestion.aspect_ratio, crop_suggestion.bounding_box))
+
+    if result.text is not None:
+        print(" Text:")
+        for line in result.text.lines:
+            points_string = "{" + ", ".join([str(int(point)) for point in line.bounding_polygon]) + "}"
+            print("   Line: '{}', Bounding polygon {}".format(line.content, points_string))
+            for word in line.words:
+                points_string = "{" + ", ".join([str(int(point)) for point in word.bounding_polygon]) + "}"
+                print("     Word: '{}', Bounding polygon {}, Confidence {:.4f}"
+                      .format(word.content, points_string, word.confidence))
+
+    result_details = sdk.ImageAnalysisResultDetails.from_result(result)
+    print(" Result details:")
+    print("   Image ID: {}".format(result_details.image_id))
+    print("   Result ID: {}".format(result_details.result_id))
+    print("   Connection URL: {}".format(result_details.connection_url))
+    print("   JSON result: {}".format(result_details.json_result))
+
+else:
+
+    error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
+    print(" Analysis failed.")
+    print("   Error reason: {}".format(error_details.reason))
+    print("   Error code: {}".format(error_details.error_code))
+    print("   Error message: {}".format(error_details.message))
+# </analyze>
