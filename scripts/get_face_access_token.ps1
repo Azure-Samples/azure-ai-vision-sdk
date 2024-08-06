d299594eddcfdb687929a3ca3e916e6a2251ed68
Nuo Wen Lei <t-nuowenlei@microsoft.com>
2024-08-06 10:05:20 -0700

    update windows face access token script

--- scripts/get_face_access_token.ps1
+++ scripts/get_face_access_token.ps1
@@ -14,24 +14,8 @@ try {
     exit 1
 }
 
-# Query the token
-try {
-    $response = Invoke-WebRequest -Uri "https://face-sdk-gating-helper-2.azurewebsites.net/sdk/subscriptions/${subscriptionId}/tokens?id=${tokenId}" -Method GET -Headers @{"Authorization"="Bearer ${bearerToken}"}
-    $statusCode = $response.StatusCode
-    if ($statusCode -eq 200) {
-        Write-Host "Token exists."
-        $response.Content | ConvertFrom-Json | Format-List
-    } else {
-        Write-Host "Token does not exist. Status code: $statusCode"
-        Write-Host "Generating a new token..."
-        $response = Invoke-RestMethod -Uri "https://face-sdk-gating-helper-2.azurewebsites.net/sdk/subscriptions/${subscriptionId}/tokens?id=${tokenId}" -Method POST -Headers @{"Authorization"="Bearer ${bearerToken}"}
-        Write-Host "Token generated successfully."
-        $response | Format-List
-    }
-} catch {
-    Write-Host "Error: $_.Exception.Message"
-    Write-Host "Generating a new token..."
-    $response = Invoke-RestMethod -Uri "https://face-sdk-gating-helper-2.azurewebsites.net/sdk/subscriptions/${subscriptionId}/tokens?id=${tokenId}" -Method POST -Headers @{"Authorization"="Bearer ${bearerToken}"}
-    Write-Host "Token generated successfully."
-    $response | Format-List
-}
+# Generate the token
+Write-Host "Generating a new token..."
+$response = Invoke-RestMethod -Uri "https://face-sdk-gating-helper-2.azurewebsites.net/sdk/subscriptions/${subscriptionId}/tokens?id=${tokenId}" -Method POST -Headers @{"Authorization"="Bearer ${bearerToken}"}
+Write-Host "Token generated successfully."
+$response | Format-List
