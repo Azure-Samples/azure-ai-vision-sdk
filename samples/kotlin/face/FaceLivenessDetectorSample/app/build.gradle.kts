fe3d568489e59838c05c34406b6571943769487d
Lingbing Jiang <lingbing.jiang@microsoft.com>
2024-10-22 14:04:35 -0700

    refine the sample

--- samples/kotlin/face/FaceLivenessDetectorSample/app/build.gradle.kts
+++ samples/kotlin/face/FaceLivenessDetectorSample/app/build.gradle.kts
@@ -8,10 +8,6 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization")
 }
-val keystorePropertiesFile = rootProject.file("keystore.properties")
-val keystoreProperties = Properties()
-keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-
 android {
     namespace = "com.example.facelivenessdetectorsample"
     compileSdk = 34
@@ -23,7 +19,7 @@ android {
         versionCode = 1
         versionName = "1"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -63,10 +59,4 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.0-alpha08")
     implementation("com.azure.ai:azure-ai-vision-face-ui:0.18.0-beta.1")
     implementation("com.azure.android:azure-core-http-httpurlconnection:1.0.0-beta.10")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.03"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
