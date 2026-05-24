ed3ad5a48ea81e3a1c90d55b22e91e865adee549
REDMOND\lingj <Lingbing.Jiang@microsoft.com>
2026-05-24 13:40:39 -0700

    update public sample

--- samples/kotlin/face/FaceLivenessDetectorFeatureSample/dynamicfeature/build.gradle.kts
+++ samples/kotlin/face/FaceLivenessDetectorFeatureSample/dynamicfeature/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.dynamic-feature")
-    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 android {
     namespace = "com.microsoft.azure.ai.vision.facelivenessdetectorsample.dynamicfeature"
@@ -14,14 +14,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
     }
     buildTypes {
         release {
--- samples/kotlin/face/FaceLivenessDetectorSample/build.gradle.kts
+++ samples/kotlin/face/FaceLivenessDetectorSample/build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10" apply false
+    id("com.android.application") version "9.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
 }
